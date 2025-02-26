972b8b568410bfa622ad7ba35932ae6917169df0
AmalAlhammadiSecurrency <Amal.alhammadi@securrency.com>
2025-02-26 13:53:15 +0400

    This is a signed commit

--- obj/Debug/net9.0/ZscalerAppMVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/ZscalerAppMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZscalerAppMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+429446261ef75362afaec0c5cbf874099808c868")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+635fbf8cd7779150c2d0461c2a79185624a7f3e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZscalerAppMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZscalerAppMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
