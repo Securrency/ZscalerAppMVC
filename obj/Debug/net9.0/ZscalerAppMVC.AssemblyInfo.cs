722a44b1caa6ee95f9e40bb8b60a1f20515e48dc
AmalAlhammadiSecurrency <Amal.alhammadi@securrency.com>
2025-04-23 09:50:20 +0400

    Changed Index Interface

--- obj/Debug/net9.0/ZscalerAppMVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/ZscalerAppMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZscalerAppMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+682310b5992e6316d12ff49753d15000b1bb14bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+525b32efd3adba64a7615b4060f0db27c5cb0413")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZscalerAppMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZscalerAppMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
